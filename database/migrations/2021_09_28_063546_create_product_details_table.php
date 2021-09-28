fb42bad684aaed0a49b2180e74888b6ca534c80c
wisnubaldas <wisnubaldas@gmail.com>
2021-09-28 16:04:00 +0700

    first commit

---
+++ database/migrations/2021_09_28_063546_create_product_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('kalori');
+            $table->string('protein');
+            $table->string('karbohidrat');
+            $table->string('lemak');
+            $table->string('komposisi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_details');
+    }
+}
---
+++ database/migrations/2021_09_28_063603_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('customer_id');
+            $table->integer('pelayan_id');
+            $table->dateTime('start');
+            $table->string('status');
+            $table->string('meja');
+            $table->string('rating')->nullable();
+            $table->string('komentar')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
