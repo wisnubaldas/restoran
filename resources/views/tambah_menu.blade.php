2aa3aee9e355e8e701a38340849b1c8dd890a6a4
Muhamadfahru90 <fabulousvit@gmail.com>
2022-08-12 21:45:00 +0700

    first commit development

--- resources/views/tambah_menu.blade.php
+++ resources/views/tambah_menu.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'AdminLTE')
 
 @section('content_header')
-    <h1 class="m-0 text-dark">Menu Pesanan</h1>
+    <h1 class="m-0 text-dark">Tambah Menu Pesanan</h1>
 @stop
 
 @section('content')
